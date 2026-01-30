9cbb96114dd219a8cc102d2391602c039f95bac8
shaqeelless4 <shaqeelless4@gmail.com>
2026-01-30 22:21:38 +0000

    update changes.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 import 'firebase_options.dart'; // Run: flutterfire configure
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
